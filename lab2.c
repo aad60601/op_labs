0d2db3a701cb9ec5bff4bca8e6ef6493200bbee5
user <you@example.com>
2021-04-20 14:01:51 +0500

    лаба 3

---
+++ lab2.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+ #include <math.h>
+ int main()
+ {
+    double x;
+    printf("Enter x {(x > 0.000001 && x <= 0.1 )} -> ");
+    scanf("%lf", &x);
+    if (x > 0.000001 && x <= 0.1)
+    {
+        double y = 5 * sqrt(sin(sqrt(x)));
+        double z = log10(cos(log(y)));
+        printf("y(x) = %lf\nz(y) = %lf\n", y, z);
+    }
+    else
+    {
+        printf("x value is incorrect!/n");
+    }
+    return 0;
+ }
